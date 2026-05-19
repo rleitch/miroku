72eb3dc31c5657cb924c017a168c0fc996ba7d48
Rick Leitch <github@rickleitch.com>
2026-05-18 21:33:12 -0500

    Stub out project (#4)
    * initial checkin
    
    * Almost Done
    
    * Trying to trigger a build
    
    * remvoing yaml
    
    * csproj tweak
    
    * Configuring ollama client with api key

---
+++ src/Miroku.Ollama/OllamaSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Miroku.Ollama;
+
+public class OllamaSettings
+{
+    public string BaseAddress { get; set; } = "http://localhost:11434/";
+
+    public string ApiKey { get; set; } = string.Empty;
+}
---
+++ src/Miroku.Web/Configuration/Settings.cs
@@ -0,0 +1,8 @@
+﻿using Miroku.Ollama;
+
+namespace Miroku.Web.Configuration;
+
+public class Settings
+{
+    public OllamaSettings OllamaSettings { get; set; } = new OllamaSettings();
+}

5bdfecd90414b529ff29411244f79e3d36ad2ebb
Rick Leitch <github@rickleitch.com>
2026-08-09 11:39:39 -0500

    testing gemma 2 model

--- src/Miroku.Ollama/Models/Requests/BaseRequest.cs
+++ src/Miroku.Ollama/Models/Requests/BaseRequest.cs
@@ -5,7 +5,7 @@ namespace Miroku.Ollama.Models.Requests;
 public abstract class BaseRequest
 {
     [JsonPropertyName("model")]
-    public string Model { get; set; } = "gemma4:e4b";
+    public string Model { get; set; } = "gemma2:9b";
 
     [JsonPropertyName("stream")]
     public bool Stream { get; set; } = true;

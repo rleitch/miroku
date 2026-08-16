bf1a0f2d28d3d153a39a091934fad902c0a2ee6e
Rick Leitch <github@rickleitch.com>
2026-08-16 14:03:08 -0500

    Increase MaxConversationSize from 8192 to 16384

--- src/Miroku.Ollama/Models/Requests/BaseRequest.cs
+++ src/Miroku.Ollama/Models/Requests/BaseRequest.cs
@@ -31,7 +31,7 @@ public abstract class BaseRequest
         public int TopK { get; set; } = 40;
 
         [JsonPropertyName("num_ctx")]
-        public int MaxConversationSize { get; set; } = 8192;
+        public int MaxConversationSize { get; set; } = 16384;
 
         [JsonPropertyName("num_predict")]
         public int MaxResponseSize { get; set; } = 4096;
